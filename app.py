41308bef6a673cb6bc3dd3fbbf0fb0a2eda02a9a
Natalie Dreher <n-dreher@hotmail.com>
2023-05-03 22:48:13 -0600

    added way to much for one commit, oops

--- app.py
+++ app.py
@@ -1,6 +1,15 @@
 import sqlite3
 
 from flask import Flask, redirect, g, url_for, request, render_template, make_response, session, flash
+from wtforms import Form, StringField, SelectField, SubmitField
+from wtforms.validators import DataRequired
+
+class SearchForm(Form):
+    search = StringField('Player Search', [DataRequired()])
+    submit = SubmitField('Search', render_kw={'class': 'btn btn-success btn-block'})
+
+
+
 
 app = Flask(__name__)
 
@@ -23,9 +32,202 @@ app.wsgi_app = PrefixMiddleware(app.wsgi_app)
 
 
 def get_db_connection():
-    conn = sqlite3.connect('database.db')
+    conn = sqlite3.connect('hockey.db')
+    c = conn.cursor()
     return conn
 
 @app.route('/')
 def index():
     return make_response(render_template("index.html"))
+
+@app.route('/games')
+@app.route('/games/<int:season>')
+@app.route('/games/<int:season>/<string:team1>')
+@app.route('/games/<int:season>/<string:team1>/<string:team2>')
+def games(season=None, team1=None, team2=None):
+    gamesLst = []
+    teamsLst = ['ANA','ARI','BOS','BUF','CAR','CBJ','CGY','CHI','COL','DAL','DET','EDM','FLA','LAK','MIN','MTL','NJD','NSH','NYI','NYR','OTT','PHI','PIT','SEA','SJS','STL','TBL','TOR','VAN','VGK','WPG','WSH']
+    if (team2):
+        conn = sqlite3.connect('hockey.db')
+        c = conn.cursor()
+        c.execute("SELECT * FROM games WHERE season=? AND (team=? AND opposingTeam=?) ORDER BY gameDate DESC;", (str(season), team1, team2))
+        gamesLst = c.fetchall()
+        conn.commit()
+        conn.close()
+    return make_response(render_template("games.html", season=season, team1=team1, team2=team2, gamesLst=gamesLst, teamsLst=teamsLst))
+
+@app.route('/goalies')
+@app.route('/goalies/<string:team1>')
+def goalies(team1=None):
+    goalieLst = []
+    if (team1):
+        conn = sqlite3.connect('hockey.db')
+        c = conn.cursor()
+        c.execute("SELECT * FROM goalies WHERE team=?;", (team1, ))
+        goalieLst = c.fetchall()
+        conn.commit()
+        conn.close()
+    return make_response(render_template("goalies.html", team1=team1, goalieLst=goalieLst))
+
+@app.route('/lines')
+@app.route('/lines/<string:team1>')
+def lines(team1=None):
+    linesLst = []
+    if (team1):
+        conn = sqlite3.connect('hockey.db')
+        c = conn.cursor()
+        c.execute("SELECT * FROM lines WHERE team=?;", (team1, ))
+        linesLst = c.fetchall()
+        conn.commit()
+        conn.close()
+    return make_response(render_template("lines.html", team1=team1, linesLst=linesLst))
+
+@app.route('/skaters')
+@app.route('/skaters/<string:team1>')
+def skaters(team1=None):
+    skaterLst = []
+    if (team1):
+        conn = sqlite3.connect('hockey.db')
+        c = conn.cursor()
+        c.execute("SELECT * FROM skaters WHERE team=?;", (team1, ))
+        skaterLst = c.fetchall()
+        conn.commit()
+        conn.close()
+    return make_response(render_template("skaters.html", team1=team1, skaterLst=skaterLst))
+
+
+@app.route('/search', methods=['GET', 'POST'])
+def search():
+    form = SearchForm(request.form)
+    resLst = []
+    if request.method == 'POST':
+        conn = sqlite3.connect('hockey.db')
+        c = conn.cursor()
+        search_string = form.data['search']
+
+        if len(search_string) < 3:
+            return render_template('search.html', form=form, foundRes=True, resLst=[], tooShort=True)
+        
+        nameQuery = "SELECT name, playerId FROM skaters WHERE situation='all' AND name LIKE '%" + search_string + "%';"
+        c.execute(nameQuery)
+        resLst = c.fetchall()
+        conn.commit()
+        conn.close()
+
+        if len(resLst) > 1:
+            return render_template('search.html', form=form, foundRes=True, resLst=resLst, tooShort=False)
+        
+        elif len(resLst) == 1:
+            url = '/player/' + str(resLst[0][1])
+            return redirect(url)
+        
+        else:
+            return render_template('search.html', form=form, foundRes=False, resLst=resLst, tooShort=False)
+
+    return render_template('search.html', form=form, foundRes=True, resLst=resLst, tooShort=False)
+
+
+query = '''
+SELECT s1.name, s1.team, s1.season, s1.position, s1.Assists, 
+(COUNT(CASE WHEN s2.Assists > s1.Assists THEN 1 END)+1) AS aRank,
+    s1.goals, (COUNT(CASE WHEN s2.goals > s1.goals THEN 1 END)+1) AS gRank,
+    s1.points, (COUNT(CASE WHEN s2.points > s1.points THEN 1 END)+1) AS pRank,
+    s1.penalties, s1.penalityMinutes, s1.hits, s1.penaltiesDrawn, s1.shotsBlockedByPlayer, s1.PlusMinus 
+FROM (SELECT name, season, team, position, (primaryAssists + secondaryAssists) AS Assists, goals, points, 
+        penalties, penalityMinutes, hits, penaltiesDrawn, shotsBlockedByPlayer,
+        (OnIce_F_goals - OnIce_A_goals) AS PlusMinus 
+    FROM skaters WHERE situation="all" AND playerId=?) s1, 
+(SELECT (primaryAssists + secondaryAssists) AS Assists, goals, points, 
+    penalties, penalityMinutes, hits, penaltiesDrawn, shotsBlockedByPlayer, 
+    (OnIce_F_goals - OnIce_A_goals) AS PlusMinus 
+    FROM skaters WHERE situation="all") s2;
+'''
+@app.route('/player')
+@app.route('/player/<int:id1>')
+@app.route('/player/<int:id1>/<int:id2>')
+def player(id1=None, id2=None):
+    playerLst1 = []
+    playerLst2 = []
+    bestLine1 = []
+    bestLine2 = []
+    if (id1):
+        conn = sqlite3.connect('hockey.db')
+        c = conn.cursor()
+        c.execute(query, (str(id1), ))
+        playerLst1 = c.fetchall()
+        fullName = playerLst1[0][0].split()
+        nameQuery = "SELECT name, MAX(goalsFor) FROM lines WHERE name LIKE '%" + fullName[-1] + "%';"
+        c.execute(nameQuery)
+        bestLine1 = c.fetchall()
+        conn.commit()
+        conn.close()
+
+    if (id2):
+        conn = sqlite3.connect('hockey.db')
+        c = conn.cursor()
+        c.execute(query, (str(id2), ))
+        playerLst2 = c.fetchall()
+        fullName = playerLst2[0][0].split()
+        nameQuery = "SELECT name, MAX(goalsFor) FROM lines WHERE name LIKE '%" + fullName[-1] + "%';"
+        c.execute(nameQuery)
+        bestLine2 = c.fetchall()
+        conn.commit()
+        conn.close()
+
+    return make_response(render_template("player.html", id1=id1, playerLst1=playerLst1, id2=id2, playerLst2=playerLst2, bestLine1=bestLine1, bestLine2=bestLine2))
+
+
+@app.route('/leaders')
+@app.route('/leaders/<string:stat>')
+def leaders(stat=None):
+    if (not stat):
+        stat = "goals"
+
+    leaderQuery = '''
+SELECT name, team, season, position, Assists, goals, points, 
+    penalties, penalityMinutes, hits, penaltiesDrawn, shotsBlockedByPlayer, PlusMinus 
+FROM (SELECT name, season, team, position, (primaryAssists + secondaryAssists) AS Assists, goals, points, 
+        penalties, penalityMinutes, hits, penaltiesDrawn, shotsBlockedByPlayer,
+        (OnIce_F_goals - OnIce_A_goals) AS PlusMinus 
+    FROM skaters WHERE situation="all") s1 ORDER BY 
+'''
+    leaderQuery = leaderQuery + stat + " DESC LIMIT 10;"
+
+    conn = sqlite3.connect('hockey.db')
+    c = conn.cursor()
+    c.execute(leaderQuery)
+    leadersLst = c.fetchall()
+    conn.commit()
+    conn.close()
+
+    return make_response(render_template("leaders.html", stat=stat, leadersLst=leadersLst))
+
+
+
+@app.route('/addgame', methods=['GET', 'POST'])
+def addgame():
+    form = SearchForm(request.form)
+    return make_response(render_template("addgame.html", stat=stat, leadersLst=leadersLst))
+
+
+
+@app.route('/usergames')
+@app.route('/usergames/<string:gameid>')
+def usergame(gameid=None):
+    return make_response(render_template("usergames.html", stat=stat, leadersLst=leadersLst))
+
+
+
+@app.route('/usergames/<string:gameid>/addplay', methods=['GET', 'POST'])
+def addplay(gameid=None):
+    search = StringField('Player Search', [DataRequired()])
+    search = StringField('Player Search', [DataRequired()])
+    search = StringField('Player Search', [DataRequired()])
+    search = StringField('Player Search', [DataRequired()])
+    submit = SubmitField('Search', render_kw={'class': 'btn btn-success btn-block'})
+
+
+    return make_response(render_template("addplays.html", stat=stat, leadersLst=leadersLst))
+
+
+
